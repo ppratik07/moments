185572daeceb2f8ebcc836675b85720c9430069d
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-04-18 18:15:23 +0530

    added toast

--- moments/app/layout.tsx
+++ moments/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+import { Toaster as Sonner } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +31,8 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <Toaster />
+          <Sonner />
           {children}
         </body>
       </html>
