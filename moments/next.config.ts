e48e0a463a95bf1f75baa0d7638da8267aab9a26
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-04-18 08:02:42 +0530

    added s3  url to access domain

--- moments/next.config.ts
+++ moments/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['images.unsplash.com','plus.unsplash.com'],
+    domains: ['images.unsplash.com','plus.unsplash.com','pub-7e95bf502cc34aea8d683b14cb66fc8d.r2.dev'],
   }
 };
 
