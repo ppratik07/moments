61088a80115e2a82a6fca70656f7b3857329e503
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-04-20 12:48:20 +0530

    adding host entry

--- moments/next.config.ts
+++ moments/next.config.ts
@@ -3,8 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['images.unsplash.com','plus.unsplash.com','pub-7e95bf502cc34aea8d683b14cb66fc8d.r2.dev'],
-  }
+    domains: [
+      "images.unsplash.com",
+      "plus.unsplash.com",
+      "pub-7e95bf502cc34aea8d683b14cb66fc8d.r2.dev",
+      "api2-u745ap.transloadit.com",
+    ],
+  },
 };
 
 export default nextConfig;
