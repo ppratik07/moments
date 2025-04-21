00a930dd46462f5e646cd56ee90d8cba1935a269
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-04-21 21:27:42 +0530

    adding host entry

--- moments/next.config.ts
+++ moments/next.config.ts
@@ -9,7 +9,9 @@ const nextConfig: NextConfig = {
       "pub-7e95bf502cc34aea8d683b14cb66fc8d.r2.dev",
       "api2-u745ap.transloadit.com",
       "api2-u858ap.transloadit.com",
-      "api2-u638ap.transloadit.com"
+      "api2-u638ap.transloadit.com",
+      "memorylane.db134517dd79f4a26d091b4dcda7e499.r2.cloudflarestorage.com",
+      "db134517dd79f4a26d091b4dcda7e499.r2.cloudflarestorage.com"
     ],
   },
 };
