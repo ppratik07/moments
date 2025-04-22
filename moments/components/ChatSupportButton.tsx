47b0b96b85002bdda1056ed580461c5082f0aa76
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-04-22 16:34:29 +0530

    fixing the submission of modal

--- moments/components/ChatSupportButton.tsx
+++ moments/components/ChatSupportButton.tsx
@@ -5,7 +5,7 @@ export default function ChatSupportButton({ title }: {
 }) {
     return (
         <div>
-            <Button variant="outline" className="text-sm border-primary text-primary hover:bg-primary/10">
+            <Button type="button" variant="outline" className="text-sm border-primary text-primary hover:bg-primary/10">
                 {title}
             </Button>
         </div>
