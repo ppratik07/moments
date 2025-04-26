cbc7dd9eb1dd767b945c00ff5cd629926ea897af
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-04-26 18:33:26 +0530

    updating types

--- server/types/types.d.ts
+++ server/types/types.d.ts
@@ -1,8 +1,8 @@
 // Extend the Request interface to include userId
 export declare global {
-    namespace Express {
-        interface Request {
-            userId?: string;
-        }
+  namespace Express {
+    interface Request {
+      userId?: string;
     }
+  }
 }
