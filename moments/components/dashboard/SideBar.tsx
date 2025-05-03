06583454fd4c0a0e86722c3ba7e6f50e136dca4e
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-05-03 20:15:59 +0530

    sidebar

--- moments/components/dashboard/SideBar.tsx
+++ moments/components/dashboard/SideBar.tsx
@@ -1,17 +1,18 @@
 import { Album, BringToFront, LayoutDashboard, Settings, SlidersHorizontal, UsersRound } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Sidebar({ imageKey }: { imageKey: string }) {
+export default function Sidebar({ imageKey, projectId }: { imageKey: string; projectId?: string }) {
   const baseUrl = 'https://pub-7e95bf502cc34aea8d683b14cb66fc8d.r2.dev/memorylane';
   const imageUrl = `${baseUrl}/${imageKey}`;
 
   const navItems = [
-    { icon: <LayoutDashboard size={18} />, label: "Dashboard" },
-    { icon: <SlidersHorizontal size={18} />, label: "Contributions" },
-    { icon: <UsersRound size={18} />, label: "Invite" },
-    { icon: <Album size={18} />, label: "Memory Book" },
-    { icon: <BringToFront size={18} />, label: "Orders" },
-    { icon: <Settings size={18} />, label: "Settings" },
+    { icon: <LayoutDashboard size={18} />, label: "Dashboard", href: `/project/${projectId}` },
+    { icon: <SlidersHorizontal size={18} />, label: "Contributions", href: `/project/${projectId}/contributions` },
+    { icon: <UsersRound size={18} />, label: "Invite", href: "/dashboard/invite" },
+    { icon: <Album size={18} />, label: "Memory Book", href: "/dashboard/memory-book" },
+    { icon: <BringToFront size={18} />, label: "Orders", href: "/dashboard/orders" },
+    { icon: <Settings size={18} />, label: "Settings", href: "/dashboard/settings" },
   ];
 
   return (
@@ -26,14 +27,14 @@ export default function Sidebar({ imageKey }: { imageKey: string }) {
 
       <nav className="space-y-3">
         {navItems.map((item, idx) => (
-          <a
+          <Link
             key={idx}
-            href="#"
+            href={item.href}
             className="flex items-center gap-3 text-gray-700 hover:text-blue-700"
           >
             {item.icon}
             <span>{item.label}</span>
-          </a>
+          </Link>
         ))}
       </nav>
     </aside>
