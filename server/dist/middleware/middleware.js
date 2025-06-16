57da49f1503d7e68c4baed4623a56160f22de842
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-06-16 22:18:01 +0530

    Change in js

--- server/dist/middleware/middleware.js
+++ server/dist/middleware/middleware.js
@@ -12,7 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.authMiddleware = void 0;
+exports.authMiddleware = authMiddleware;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
@@ -65,4 +65,3 @@ function authMiddleware(req, res, next) {
         }
     });
 }
-exports.authMiddleware = authMiddleware;
